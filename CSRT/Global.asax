19ea7c899ccbcf827b54a780391183bd27cca0e5
tobi new <tmaxnoda@gmail.com>
2017-05-10 10:50:30 +0100

    Add project files.

---
+++ CSRT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSRT.MvcApplication" Language="C#" %>
---
+++ CSRT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSRT.Startup))]
+namespace CSRT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
